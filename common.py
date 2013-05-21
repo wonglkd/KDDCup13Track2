b35721f5cc90eeb946844031fc5aca61017015d4
Daniel Wong <wonglkd@gmail.com>
2013-05-22 01:01:13 +0800

    DB methods

--- common.py
+++ common.py
@@ -1,5 +1,16 @@
+import sqlite3
 import sys
 import exceptions
+
+def getDB():
+	db_filename = "kddcup13.sqlite3"
+	conn = sqlite3.connect(db_filename)
+	return conn
+
+def selectDB(conn, query, para = None):
+	cur = conn.cursor()
+	cur.execute(query, para)
+	return cur.fetchall()
 
 def print_err(*args):
     sys.stderr.write(' '.join(map(str,args)) + '\n')
