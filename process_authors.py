da2c14dac05da18a23a54d3b225c1b7aec037247
Daniel Wong <wonglkd@gmail.com>
2013-05-22 01:02:54 +0800

    Reorganised

--- process_authors.py
+++ process_authors.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 # Given authors, generate features for the individual authors
+from common import *
 import argparse
 import csv
 import math
 import numpy as np
 import cPickle as pickle
 from nameparser import HumanName
+import nameparser.constants
 from collections import defaultdict
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, ENGLISH_STOP_WORDS
 from unidecode import unidecode
-#from pprint import pprint
+from pprint import pprint
 
 def loadAuthors(authorfile):
 	reader = csv.reader(open(authorfile, 'rb'))
@@ -18,71 +20,81 @@ def loadAuthors(authorfile):
 	lastname_cnt = defaultdict(int)
 	iFfL_cnt = defaultdict(int)
 	affiliations = []
- 	print "Parsing names and counts"
+ 	print_err("Parsing names and counts")
+	punc = '.; '
+	titles_c = nameparser.constants.TITLES - set(['wing'])
 	for line in reader:
-		line[1:] = [unicode(cell, 'utf-8') for cell in line[1:]]
-		line[1], line[2] = unidecode(line[1]), unidecode(line[2])
-		hn = HumanName(line[1])
+		line[1:] = [unidecode(unicode(cell, 'utf-8')) for cell in line[1:]]
+		hn = HumanName(line[1], titles_c=titles_c)
+		hn.title = hn.title.lower().strip(punc)
+		hn.first = hn.first.lower().strip(punc)
+		hn.middle = hn.middle.lower().strip(punc)
+		hn.last = hn.last.lower().strip(punc)
+		hn.suffix = hn.suffix.lower().strip(punc)
+		hn.full_name = hn.full_name.strip().replace(';','').lower()
+		full_parsed = hn.first + ' '
+		if hn.middle:
+			full_parsed += hn.middle + ' '
+		full_parsed += hn.last + ' ' + hn.suffix
+		full_parsed = full_parsed.strip()
 		if hn.last:
 			if hn.first:
-				iFfL = hn.first[0] + ' ' + hn.last.strip('.')
-				fFfL = hn.first + ' ' + hn.last.strip('.')
-				fFfL = fFfL.strip().lower()
+				iFfL = hn.first[0] + ' ' + hn.last
 			else:
-				iFfL = 'L:' + hn.last.strip('.')
-				fFfL = iFfL
+				iFfL = 'L:' + hn.last
 		elif hn.first:
 			iFfL = 'F:' + hn.first # use full first name if no last name
-			fFfL = iFfL
 		else:
 			iFfL = 'ID:' + line[0]
+		if hn.last and hn.first:
+			fFfL = hn.first + ' ' + hn.last
+		else:
 			fFfL = iFfL
-		iFfL = iFfL.strip().lower()
 
-		full_name = hn.full_name.strip().lower()
-		if full_name == '':
-			full_name = 'ID: ' + line[0]
+		if not hn.full_name:
+			hn.full_name = 'ID:' + line[0]
+		if not full_parsed:
+			full_parsed = hn.full_name
 
 		authors.append((int(line[0]), {
- 			'name': full_name,
- 			'name_title': hn.title.lower().strip('.'),
- 			'name_first': hn.first.lower().strip('.'),
- 			'name_middle': hn.middle.lower().strip('.'),
- 			'name_last': hn.last.lower().strip('.'),
- 			'name_suffix': hn.suffix.lower().strip('.'),
+ 			'name': hn.full_name,
+ 			'name_title': hn.title,
+ 			'name_first': hn.first,
+ 			'name_middle': hn.middle,
+ 			'name_last': hn.last,
+ 			'name_suffix': hn.suffix,
+ 			'fullparsedname': full_parsed,
 			'iFfL': iFfL,
 			'fFfL': fFfL,
 			'affiliation': line[2]
 		}))
+ 		lastname_cnt[hn.last] += 1
+  		iFfL_cnt[iFfL] += 1
 		affiliations.append(line[2])
 		if len(affiliations) % 10000 == 0:
 			print len(affiliations)
- 		lastname_cnt[hn.last.strip('.').lower()] += 1
-  		iFfL_cnt[iFfL] += 1
-# 	stopwordlist = ['this', 'supported', 'the', 'by']
-	print "Computing Counts of affiliations"
- 	count_vec = CountVectorizer(min_df=1, binary=True)
+
+	print_err("Computing Counts of affiliations")
+ 	stopwordlist = ['a', 'an', 'and', 'at', 'by', 'department', 'of', 'supported', 'the', 'this']
+ 	stopwordlist = list(ENGLISH_STOP_WORDS | set(stopwordlist))
+ 	count_vec = CountVectorizer(min_df=1, binary=True, stop_words=stopwordlist)
  	affil_count = count_vec.fit_transform(affiliations)
-	print "Computing TF-IDF of affiliations"
- 	tt = TfidfTransformer(smooth_idf=True, sublinear_tf=False)
+
+	print_err("Computing TF-IDF of affiliations")
+ 	tt = TfidfTransformer(use_idf=True, smooth_idf=True, sublinear_tf=False)
  	affil_tfidf = tt.fit_transform(affil_count)
-#	print l
 #    	print count_vec.inverse_transform(affil_tfidf)
 #   	pprint(count_vec.get_feature_names())
-	print "Calculating IDFs"
-# 	iFfL_IDF = np.log(float(len(authors)) / np.array(iFfL_cnt.values()))
-# 	iFfL_IDF = dict(zip(iFfL_cnt.keys(), iFfL_IDF / iFfL_IDF.max()))
-# 	lastname_IDF = np.log(float(len(authors)) / np.array(lastname_cnt.values()))
-# 	lastname_IDF = dict(zip(lastname_cnt.keys(), lastname_IDF / lastname_IDF.max()))
 
+	print_err("Calculating IDFs")
 	iFfL_IDF = dict(zip(iFfL_cnt.keys(), np.log(float(len(authors)) / np.array(iFfL_cnt.values()))))
 	lastname_IDF = dict(zip(lastname_cnt.keys(), np.log(float(len(authors)) / np.array(lastname_cnt.values()))))
 
+	print_err("Packing it into a list")
 	affil_count = affil_count.tocsr()
-	print "Packing it into a list"
  	for i, a in enumerate(authors):
  		authors[i][1]['iFfL_idf'] = iFfL_IDF[a[1]['iFfL']]
- 		authors[i][1]['lastname_idf'] = lastname_IDF[a[1]['name_last'].lower()]
+ 		authors[i][1]['lastname_idf'] = lastname_IDF[a[1]['name_last']]
  		if a[1]['affiliation'] == '':
  			authors[i][1]['affil_tdidf'], authors[i][1]['affil_count'] = None, None
  		else:
@@ -100,8 +112,8 @@ def main():
 	args = parser.parse_args()
 	
 	authors = loadAuthors(args.authorfile)
-	print "Pickling..."
-	pickle.dump(authors, open(args.outputfile, 'wb'))
+	print_err("Pickling...")
+	pickle.dump(authors, open(args.outputfile, 'wb'), pickle.HIGHEST_PROTOCOL)
 
 if __name__ == "__main__":
 	main()
