2e6153b3948a83f6b0a2e5d36299307ca52dfe3a
Daniel Wong <wonglkd@gmail.com>
2013-05-24 15:07:14 +0800

    only calculated TF-IDF for those with affiliations

--- process_authors.py
+++ process_authors.py
@@ -23,8 +23,10 @@ def loadAuthors(authorfile):
  	print_err("Parsing names and counts")
 	punc = '.; '
 	titles_c = nameparser.constants.TITLES - set(['wing'])
-	for line in reader:
+	id2affiliation = {}
+	for i, line in enumerate(reader):
 		line[1:] = [unidecode(unicode(cell, 'utf-8')) for cell in line[1:]]
+		line[2] = line[2].lower()
 		hn = HumanName(line[1], titles_c=titles_c)
 		hn.title = hn.title.lower().strip(punc)
 		hn.first = hn.first.lower().strip(punc)
@@ -70,19 +72,25 @@ def loadAuthors(authorfile):
 		}))
  		lastname_cnt[hn.last] += 1
   		iFfL_cnt[iFfL] += 1
-		affiliations.append(line[2])
-		if len(affiliations) % 10000 == 0:
-			print len(affiliations)
+  		if line[2]:
+			id2affiliation[int(line[0])] = len(affiliations)
+			affiliations.append(line[2])
+		if (i+1) % 10000 == 0:
+			print i+1, "rows processed"
 
 	print_err("Computing Counts of affiliations")
- 	stopwordlist = ['a', 'an', 'and', 'at', 'by', 'department', 'of', 'supported', 'the', 'this']
+ 	stopwordlist = ['a', 'an', 'and', 'at', 'by', 'department', 'of', 'supported', 'the', 'this', 'school']
  	stopwordlist = list(ENGLISH_STOP_WORDS | set(stopwordlist))
  	count_vec = CountVectorizer(min_df=1, binary=True, stop_words=stopwordlist)
  	affil_count = count_vec.fit_transform(affiliations)
 
 	print_err("Computing TF-IDF of affiliations")
- 	tt = TfidfTransformer(use_idf=True, smooth_idf=True, sublinear_tf=False)
+ 	tt = TfidfTransformer(norm=None, use_idf=True, smooth_idf=True, sublinear_tf=False)
  	affil_tfidf = tt.fit_transform(affil_count)
+#  	print affil_tfidf.shape
+#  	for i in range(10):
+#  		print i
+#  		print affil_tfidf[:,i]
 #    	print count_vec.inverse_transform(affil_tfidf)
 #   	pprint(count_vec.get_feature_names())
 
@@ -95,11 +103,12 @@ def loadAuthors(authorfile):
  	for i, a in enumerate(authors):
  		authors[i][1]['iFfL_idf'] = iFfL_IDF[a[1]['iFfL']]
  		authors[i][1]['lastname_idf'] = lastname_IDF[a[1]['name_last']]
- 		if a[1]['affiliation'] == '':
- 			authors[i][1]['affil_tdidf'], authors[i][1]['affil_count'] = None, None
+ 		if len(a[1]['affiliation']) == 0:
+ 			authors[i][1]['affil_tdidf'] = None
+#  			authors[i][1]['affil_count'] = None
  		else:
-			authors[i][1]['affil_tdidf'] = affil_tfidf[i]
- 			authors[i][1]['affil_count'] = affil_count[i]
+			authors[i][1]['affil_tdidf'] = affil_tfidf[id2affiliation[a[0]]]
+ # 			authors[i][1]['affil_count'] = affil_count[id2affiliation[a[0]]]
 		if (i+1) % 10000 == 0:
 			print i+1
  	authors_dict = dict(authors)
