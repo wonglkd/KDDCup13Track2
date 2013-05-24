81c8fbb35fa52349219cdf1fb01b4ff6f1bb1059
Daniel Wong <wonglkd@gmail.com>
2013-05-25 01:18:36 +0800

    Coauthors query

--- queries.sql
+++ queries.sql
@@ -15,7 +15,7 @@ GROUP BY AuthorId, LOWER(affiliation);
 .output pa_years.csv
 SELECT pa.AuthorId, p.year, COUNT(*) FROM paper p JOIN paperauthor pa ON p.Id = pa.paperId
 JOIN awithpapers awp ON awp.Id=AuthorId
-WHERE p.year > 0
+WHERE p.year >= 1600 AND p.year <= 2013
 GROUP BY AuthorId, p.year;
 
 .output pa_conferences.csv
@@ -36,20 +36,76 @@ FROM paperauthor pa1 JOIN paperauthor pa2 ON pa1.PaperId = pa2.PaperId
 JOIN awithpapers a ON a.Id = pa1.AuthorId
 GROUP BY pa1.AuthorId, LOWER(TRIM(REPLACE(pa2.name, ';', '')));
 
--- CREATE TABLE pa_coauthors (AuthorId INT, Coauthor TEXT, cnt INT); 
--- CREATE pa_coauthors_
--- .import pa_coauthors.csv pa_coauthors_
+CREATE TABLE pa_coauthors (AuthorId INT, Coauthor TEXT, cnt INT, PRIMARY KEY (AuthorId, Coauthor));
 
-SELECT COUNT(*) FROM
-(SELECT Coauthor FROM pa_coauthors co1
-WHERE co1.AuthorId = ??
-INTERSECT
-SELECT Coauthor FROM pa_coauthors co2
-WHERE co2.AuthorId = ??)
-JOIN
-SELECT COUNT(Coauthor) FROM pa_coauthors
-WHERE co1.AuthorId IN (??, ??)
+INSERT INTO pa_coauthors (AuthorId, Coauthor, cnt)
+SELECT AuthorId, Coauthor, SUM(cnt) as cnt FROM pa_coauthors_ GROUP BY AuthorId, Coauthor;
 
+CREATE INDEX pa_coauthors_authorid_idx  ON pa_coauthors (AuthorId);
+
+DROP TABLE pa_coauthors_;
+
+-- CREATE INDEX pa_coauthors_idx  ON Coauthor (AuthorId, Coauthor);
+CREATE TABLE pa_coauthors_ (AuthorId INT, Coauthor TEXT, cnt INT);
+.import pa_coauthors.csv pa_coauthors_
+
+SELECT
+			(SELECT COUNT(*) FROM
+			(SELECT Coauthor FROM pa_coauthors co1
+			WHERE co1.AuthorId = 1688847
+			INTERSECT
+			SELECT Coauthor FROM pa_coauthors co2
+			WHERE co2.AuthorId = 364837)) as common
+		,
+			(SELECT COUNT(Coauthor) FROM pa_coauthors co3
+			WHERE co3.AuthorId = 1688847) as cnt1
+		,
+			(SELECT COUNT(Coauthor) FROM pa_coauthors co3
+			WHERE co3.AuthorId = 364837) as cnt2
+;
+
+SELECT
+			(SELECT IFNULL(SUM(mc),0) FROM (SELECT MIN(cnt) as mc FROM
+			pa_coauthors co0 JOIN
+			(SELECT Coauthor FROM pa_coauthors co1
+			WHERE co1.AuthorId = 1688847
+			INTERSECT
+			SELECT Coauthor FROM pa_coauthors co2
+			WHERE co2.AuthorId = 364837) co_ ON co_.Coauthor = co0.Coauthor WHERE AuthorId IN (364837,1688847)
+			GROUP BY co_.Coauthor
+			)) as common
+		,
+			(SELECT SUM(cnt) FROM pa_coauthors co3
+			WHERE co3.AuthorId = 1688847) as cnt1
+		,
+			(SELECT SUM(cnt) FROM pa_coauthors co3
+			WHERE co3.AuthorId = 364837) as cnt2
+;
+
+
+SELECT
+			(SELECT IFNULL(SUM(mc),0) FROM (SELECT MIN(cnt) as mc FROM
+			pa_coauthors co0 JOIN
+			(SELECT Coauthor FROM pa_coauthors co1
+			WHERE co1.AuthorId = 426
+			INTERSECT
+			SELECT Coauthor FROM pa_coauthors co2
+			WHERE co2.AuthorId = 1107709) co_ ON co_.Coauthor = co0.Coauthor WHERE AuthorId IN (426,1107709)
+			GROUP BY co_.Coauthor
+			)) as common
+		,
+			(SELECT SUM(cnt) FROM pa_coauthors co3
+			WHERE co3.AuthorId = 426) as cnt1
+		,
+			(SELECT SUM(cnt) FROM pa_coauthors co3
+			WHERE co3.AuthorId = 1107709) as cnt2
+;
+ 1107709
+
+
+SELECT COUNT(Coauthor) FROM pa_coauthors co3
+WHERE co3.AuthorId IN (1688847, 364837)
+GROUP BY authorid
 
 -- count number of papers that we actually need to parse
 .output stdout
