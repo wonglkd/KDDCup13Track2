33a6aca0c307b473f1876ecd9ceee26309369541
Daniel Wong <wonglkd@gmail.com>
2013-05-24 15:05:42 +0800

    Experimenting with APSW

--- common.py
+++ common.py
@@ -1,16 +1,28 @@
 import sqlite3
+import apsw
 import sys
 import exceptions
 
-def getDB():
-	db_filename = "kddcup13.sqlite3"
-	conn = sqlite3.connect(db_filename)
+def getDB(id='origdata'):
+	DBs = {
+		'origdata': "kddcup13.sqlite3",
+		'pa': 'pa.sqlite3'
+	}
+	db_filename = DBs[id]
+# 	conn = sqlite3.connect(db_filename)
+	conn = apsw.Connection(db_filename)
+	if id == 'pa':
+		memcon = apsw.Connection(":memory")
+		with memcon.backup("main", conn, "main") as backup:
+			backup.step()
+		return memcon
 	return conn
 
 def selectDB(conn, query, para = None):
 	cur = conn.cursor()
-	cur.execute(query, para)
-	return cur.fetchall()
+	for row in cur.execute(query, para):
+		yield row
+# 	return cur.fetchall()
 
 def print_err(*args):
     sys.stderr.write(' '.join(map(str,args)) + '\n')
@@ -24,4 +36,7 @@ def num(s):
     try:
         return int(s)
     except exceptions.ValueError:
-        return float(s)
+        try:
+        	return float(s)
+        except exceptions.ValueError:
+        	raise Exception("Could not convert string to number:"+ s)
