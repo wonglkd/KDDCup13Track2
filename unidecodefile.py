75252c78445a346e72dfc4fd651356f3c73ad8a9
Daniel Wong <wonglkd@gmail.com>
2013-05-28 17:13:04 +0800

    Added converting to lowercase as well

--- unidecodefile.py
+++ unidecodefile.py
@@ -12,7 +12,7 @@ f_in = open(sys.argv[1], 'rb') if sys.argv[1] != '-' else sys.stdin
 
 writer = csv.writer(f_write)
 for i, line in enumerate(csv.reader(f_in)):
-	line[1] = unidecode(unicode(line[1], 'utf-8')).strip()
+	line[1] = unidecode(unicode(line[1], 'utf-8')).strip().lower()
 # 	line = [unidecode(unicode(cell, 'utf-8')).strip() for cell in line]
 	writer.writerow(line)
 	if (i+1) % 10000 == 0:
