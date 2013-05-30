b17d56f08ca2d3882d73897aae272115c70fe6eb
Daniel Wong <wonglkd@gmail.com>
2013-05-30 14:35:56 +0800

    Generation of metaphone name, removed uni words from stopword list

--- process_authors.py
+++ process_authors.py
@@ -12,8 +12,9 @@ from collections import defaultdict
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, ENGLISH_STOP_WORDS
 from unidecode import unidecode
 from pprint import pprint
+import jellyfish
 
-def loadAuthors(authorfile):
+def loadAuthors(authorfile, printaffilwordfreq=False):
 	reader = csv.reader(open(authorfile, 'rb'))
 	reader.next()
 	authors = []
@@ -41,6 +42,7 @@ def loadAuthors(authorfile):
 		ai['fullname'] = hn.full_name.lower().encode('ascii').translate(None, punc.replace(' ',''))
 		ai['fullname_parsed'] = ai['name_first'] + ai['name_middle'] + ai['name_last'] + ai['name_suffix']
 		ai['affiliation'] = line[2].lower()
+		ai['metaphone_fullname'] = jellyfish.metaphone(ai['fullname']).encode('ascii').translate(None, ' ')
 		if ai['name_last']:
 			if ai['name_first']:
 				ai['iFfL'] = ai['name_first'][0] + ai['name_last']
@@ -71,22 +73,27 @@ def loadAuthors(authorfile):
 			id2affiliation[int(line[0])] = len(affiliations)
 			affiliations.append(line[2])
 		if (i+1) % 10000 == 0:
-			print i+1, "rows processed"
+			print_err(i+1, "rows processed")
 
 	print_err("Computing Counts of affiliations")
- 	stopwordlist = ['a', 'an', 'and', 'at', 'by', 'department', 'of', 'supported', 'the', 'this', 'school', 'institute', 'university', 'college', 'institution']
+ 	stopwordlist = ['a', 'an', 'and', 'at', 'by', 'of', 'supported', 'the', 'this']
+ 	# stopwordlist += ['department', 'university''school', 'institute', 'college', 'institution']
  	stopwordlist = list(ENGLISH_STOP_WORDS | set(stopwordlist))
- 	count_vec = CountVectorizer(min_df=1, binary=True, stop_words=stopwordlist)
+ 	# min_df = 2 because we will be looking for common words
+ 	count_vec = CountVectorizer(min_df=2, binary=True, stop_words=stopwordlist)
  	affil_count = count_vec.fit_transform(affiliations)
 
 	print_err("Computing TF-IDF of affiliations")
  	tt = TfidfTransformer(norm=None, use_idf=True, smooth_idf=True, sublinear_tf=False)
  	affil_tfidf = tt.fit_transform(affil_count)
-#  	for i in range(10):
-#  		print i
-#  		print affil_tfidf[:,i]
-#    	print count_vec.inverse_transform(affil_tfidf)
-#   	pprint(count_vec.get_feature_names())
+
+	# print words sorted by frequency
+	if printaffilwordfreq:
+		kk = zip(affil_count.sum(axis=0).tolist()[0], count_vec.get_feature_names())
+		kk = sorted(kk)
+		for a, b in kk:
+			print '{:} {:}'.format(a, b)
+		return
 
 	print_err("Calculating IDFs")
 	iFfL_IDF = dict(zip(iFfL_cnt.keys(), np.log(float(len(authors)) / np.array(iFfL_cnt.values()))))
@@ -104,17 +111,20 @@ def loadAuthors(authorfile):
 			authors[i][1]['affil_tdidf'] = affil_tfidf[id2affiliation[a[0]]]
  # 			authors[i][1]['affil_count'] = affil_count[id2affiliation[a[0]]]
 		if (i+1) % 10000 == 0:
-			print i+1
+			print_err(i+1)
  	authors_dict = dict(authors)
  	return authors_dict
 
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('authorfile')
-	parser.add_argument('outputfile')
+	parser.add_argument('outputfile', nargs='?')
+	parser.add_argument('--affilwordfreq', action='store_true')
 	args = parser.parse_args()
 	
-	authors = loadAuthors(args.authorfile)
+	authors = loadAuthors(args.authorfile, args.affilwordfreq)
+	if args.affilwordfreq:
+		return
 	print_err("Pickling...")
 	pickle.dump(authors, open(args.outputfile, 'wb'), pickle.HIGHEST_PROTOCOL)
 
