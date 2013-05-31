3e270e76d847d24bd077cc7220f505528d75d35d
Daniel Wong <wonglkd@gmail.com>
2013-05-31 11:50:49 +0800

    Added interactive option

--- analyseClusters.py
+++ analyseClusters.py
@@ -3,11 +3,23 @@ from common import *
 from pprint import pprint
 import argparse
 
+def getCluster(line, query, conn):
+	if ';' in line:
+		line = line.split(';')[1]
+	cluster = line.strip().split(',')
+	for v in cluster:
+		res = selectDB(conn, query + ' WHERE a.Id = '+v+' LIMIT 2')
+		for line in res:
+			print(line)
+	print('-----------')
+
+
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('qtype', nargs='?', default='author')
+	parser.add_argument('-c', '--rawinput', action='store_true')
 	args = parser.parse_args()
-
+	
 	queries = {
 		'author': '''SELECT * FROM author a''',
 		'all': '''SELECT a.*, pa.name, pa.affiliation, p.title, p.year, p.journalId, p.conferenceId FROM
@@ -15,17 +27,17 @@ def main():
 	LEFT JOIN paper p ON p.Id = PaperId'''
 	}
 
-	with open('analysis/analyse.clusters.tmp') as f:
-		with getDB() as conn:
-			for line in skip_comments(f):
-				if ';' in line:
-					line = line.split(';')[1]
-				cluster = line.strip().split(',')
-				for v in cluster:
-					res = selectDB(conn, queries[args.qtype] + ' WHERE a.Id = '+v+' LIMIT 2')
-					for line in res:
-						print(line)
-				print('-----------')
+	with getDB() as conn:
+		if args.rawinput:
+			while True:
+				line = raw_input("Cluster: ")
+				if not line:
+					break
+				getCluster(line, queries[args.qtype], conn)
+		else:
+			with open('analysis/analyse.clusters.tmp') as f:
+				for line in skip_comments(f):
+					getCluster(line, queries[args.qtype], conn)
 
 if __name__ == "__main__":
 	main()
