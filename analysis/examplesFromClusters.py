bcb65479b5f21d318d8b4c259ebb662e1a1d570f
Daniel Wong <wonglkd@gmail.com>
2013-06-03 22:30:25 +0800

    Added .strip()

--- analysis/examplesFromClusters.py
+++ analysis/examplesFromClusters.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 from itertools import combinations, product, chain
 
-clusters = '''721471
-1294623
-2219483
-1444079
-2072490'''
+clusters = '''1077322
+1004361
+2290355
+1030132
+341152 
+376374 
+1798702
+1211468
+1229993
+1177255
+497471 
+1145517
+1492855
+953365 
+1702128
+1557010'''
 
-clusters = '''123,456
-345,753'''
-
-clusters = [cl.split(',') for cl in clusters.split('\n')]
+clusters = [cl.strip().split(',') for cl in clusters.split('\n')]
 for cl in clusters:
 	for a1, a2 in combinations(cl, 2):
 		print ",".join(['1', a1, a2])
