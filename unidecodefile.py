5983d1760b9268086d23dd8ae561dc1980f42e3f
Daniel Wong <wonglkd@gmail.com>
2013-06-04 16:22:46 +0800

    Added feature to strip punctuation

--- unidecodefile.py
+++ unidecodefile.py
@@ -3,6 +3,7 @@ from unidecode import unidecode
 import fileinput
 import csv
 import argparse
+from process_authors import punc
 
 def main():
 	parser = argparse.ArgumentParser()
@@ -10,6 +11,7 @@ def main():
 	parser.add_argument('outfile', nargs='?', default='-')
 	parser.add_argument('-c', '--cols', nargs='*', default=[1])
 	parser.add_argument('-a', '--all-cols', action='store_true')
+	parser.add_argument('-p', '--strip-punc', action='store_true')
 	args = parser.parse_args()
 
 	f_in = open(args.infile, 'rb') if args.infile != '-' else sys.stdin
@@ -17,13 +19,18 @@ def main():
 
 	args.cols = map(int, args.cols)
 
+	reader = csv.reader(f_in)
+	if args.all_cols:
+		args.cols = range(len(reader.next()))
+		f_in.seek(0)
+	
 	writer = csv.writer(f_write)
-	for i, line in enumerate(csv.reader(f_in)):
-		if args.all_cols:
-			line = [unidecode(unicode(cell, 'utf-8')).strip().lower() for cell in line]
-		else:
+	for i, line in enumerate(reader):
+		for c in args.cols:
+			line[c] = unidecode(unicode(line[c], 'utf-8')).strip().lower()
+		if args.strip_punc:
 			for c in args.cols:
-				line[c] = unidecode(unicode(line[c], 'utf-8')).strip().lower()
+				line[c] = line[c].translate(None, punc.replace(' ',''))
 		writer.writerow(line)
 		if (i+1) % 10000 == 0:
 			print i+1, "lines done"
