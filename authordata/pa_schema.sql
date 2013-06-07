cdac7027204df4eda8062894346c195bcaec44ca
Daniel Wong <wonglkd@gmail.com>
2013-06-07 20:21:49 +0800

    Renamed column for standardisation

--- authordata/pa_schema.sql
+++ authordata/pa_schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE pa_coauthors_total (AuthorId INT PRIMARY KEY, cnt INT, total INT);
 INSERT INTO pa_coauthors_total (AuthorId, cnt, total)
 SELECT AuthorId, COUNT(*), SUM(cnt) FROM pa_coauthors GROUP BY AuthorId;
 
-CREATE TABLE pa_coauthors_ids (AuthorId INT, Coauthor_id INT, cnt INT, PRIMARY KEY (AuthorId, Coauthor_id));
+CREATE TABLE pa_coauthors_ids (AuthorId INT, Coauthor INT, cnt INT, PRIMARY KEY (AuthorId, Coauthor));
 .import pa_coauthors_ids_cnts.csv pa_coauthors_ids
 
 CREATE INDEX pa_coauthors_ids_authorid_idx  ON pa_coauthors_ids (AuthorId);
