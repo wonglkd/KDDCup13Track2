f6e1835cd87310e9cd0cdfea19d13e032cd65681
Daniel Wong <wonglkd@gmail.com>
2013-06-10 18:03:40 +0800

    Created script to compare HRC and HPC clusters

---
+++ ccluster.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# Given HPC+HRC, list HPC by HRC
+from common import *
+from collections import defaultdict
+from cluster_common import *
+import cPickle as pickle
+import argparse
+import csv
+from pprint import pprint
+import sys
+
+def skip_front(iterable):
+	for line in iterable:
+		if ';' in line:
+			yield line.split(';')[1]
+		else:
+			yield line
+
+def loadClusters(filename):
+	with open(filename, 'rb') as f:
+		print_err('Reading', filename)
+		for line in csv.reader(skip_front(skip_comments(f))):
+			yield map(int, line)
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('hpc_file')
+	parser.add_argument('hrc_file')
+	parser.add_argument('outfile', nargs='?')
+	parser.add_argument('-a', '--authorprefeat', default='generated/Author_prefeat.pickle')
+	args = parser.parse_args()
+	if args.outfile == None:
+		args.outfile = args.hpc_file.replace('.clusters','') + '.clusgrp'
+
+	hrc_of_author = {}
+		
+	for hrc_i, cl in enumerate(loadClusters(args.hrc_file)):
+		for aid in cl:
+			hrc_of_author[aid] = hrc_i
+
+	hrc_of_hpc = defaultdict(list)
+
+	for cl in loadClusters(args.hpc_file):
+		seen_hrcs = set()
+		for aid in cl:
+			if aid in hrc_of_author:
+				seen_hrcs.add(hrc_of_author[aid])
+		if len(seen_hrcs) == 1:
+			hrc_of_hpc[list(seen_hrcs)[0]].append(cl)
+#		else:
+#			print_err("Not in hierarchy:", str(len(seen_hrcs))+','+' '.join(map(str, seen_hrcs)))
+
+	fout = open(args.outfile, 'wb')
+
+	print_err("Loading pickled author pre-features")
+	authors = pickle.load(open(args.authorprefeat, 'rb'))
+#		import networkx as nx
+#		G_sim = nx.read_weighted_edgelist(skip_comments(open(args.edgelist, 'rb')), nodetype=int, delimiter=',')
+	
+	for hrc, hpcs in hrc_of_hpc.iteritems():
+		if len(hpcs) == 1:
+			continue
+		outputClusters(hpcs, fout, authors=authors)
+		fout.write("-----\n")
+
+if __name__ == "__main__":
+	main()
--- cluster_common.py
+++ cluster_common.py
@@ -7,29 +7,39 @@ def enforce_min(iterable, min_weight):
         if float(vl[2]) >= min_weight:
             yield line
 
-def outputClusters(clusters, fout, G_sim, authors, threshold_density=None):
+def outputClusters(clusters, fout, G_sim=None, authors=None, threshold_density=None):
 	clusters_o = []
 	
  	for clist in clusters:
- 		cl = G_sim.subgraph(clist)
-		cl_nodes = len(cl)
-		cl_edges = cl.number_of_edges()
-		cl_unweighted_density = nx.density(cl)
-		cl_weighted_density = cl.size(weight='weight')
-		fullnames = set([authors[v]['fullname'] for v in clist])
-		fullnames = '|'.join(sorted(fullnames))
-		lastnames = set([authors[v]['name_last'] for v in clist])
-		lastnames = '|'.join(sorted(lastnames))
+		line = []
+		if authors is not None:
+			fullnames = set([authors[v]['fullname'] for v in clist])
+			fullnames = '|'.join(sorted(fullnames))
+			lastnames = set([authors[v]['name_last'] for v in clist])
+			lastnames = '|'.join(sorted(lastnames))
+			line.append([lastnames, fullnames])
 
-		if cl_nodes != 1:
-			cl_weighted_density /= (.5 * cl_nodes * (cl_nodes - 1))
-		if threshold_density is None or cl_weighted_density >= threshold_density:
-			clusters_o.append((cl_nodes, cl_weighted_density, cl_edges, cl_unweighted_density, ','.join(map(str, sorted(clist))), fullnames, lastnames))
+		cl_nodes = len(cl)
+		if G_sim is not None:
+			cl = G_sim.subgraph(clist)
+			cl_edges = cl.number_of_edges()
+			cl_unweighted_density = nx.density(cl)
+			cl_weighted_density = cl.size(weight='weight')
+			if cl_nodes != 1:
+				cl_weighted_density /= (.5 * cl_nodes * (cl_nodes - 1))
+			line.append([cl_nodes, cl_weighted_density, cl_unweighted_density]) #cl_edges
 		else:
-			print_err(cl_nodes, cl_edges, cl_unweighted_density, cl_weighted_density, fullnames, lastnames)
+			line.append([cl_nodes])
+
+		if G_sim is None or threshold_density is None or cl_weighted_density >= threshold_density:
+			clusters_o.append([','.join(map(str, sorted(clist)))] + line)
+		else:
+			print_err(*(line[0]+line[1]))
 	
-	# size,weighted_density,lastnames,fullnames,unweighted_density;cluster
-	for cl_nodes, cl_weighted_density, cl_edges, cl_unweighted_density, clstr, fullnames, lastnames in sorted(clusters_o, reverse=True):
-		print cl_nodes, cl_edges, cl_unweighted_density, cl_weighted_density
-		fout.write('{:},{:g},{:},{:},{:g};{:}\n'.format(cl_nodes, cl_weighted_density, lastnames, fullnames, cl_unweighted_density, clstr))
-	
+	# size,weighted_density,unweighted_density,lastnames,fullnames;cluster
+	for cline in sorted(clusters_o, reverse=True):
+		fout.write(','.join(map('{:g}'.format, cline.pop())))
+		if authors is not None:
+			fout.write(','.join(cline.pop()))
+		fout.write(';')
+		fout.write('{:}\n'.format(cline.pop()))
