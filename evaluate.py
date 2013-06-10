58e54a639031af0dc05fd58a0ca11ab6c658d4c2
Daniel Wong <wonglkd@gmail.com>
2013-06-10 10:09:51 +0800

    Results for 20130609-1900/combined_gbm_kruskal

--- evaluate.py
+++ evaluate.py
@@ -25,6 +25,7 @@ def main():
 		'20130609-0220/combined_kruskal': 0.95850,
 		'20130609-0251/combined_kruskal': 0.95990,
 		'20130609-1900/combined_gbm_hc': 0.95959,
+		'20130609-1900/combined_gbm_kruskal': 0.96007,
 		'best': 0.96291,
 		'best-2nd-0.8': 0.96286
 	}
